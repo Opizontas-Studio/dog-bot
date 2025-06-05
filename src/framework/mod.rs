12bd18d319b9b02e038bf79d1eeee47d7d62e1d7
Xerxes-2 <dspxue@gmail.com>
2025-06-06 09:38:33 +1000

    fix(error-handling): improve error message formatting in on_error function

--- src/framework/mod.rs
+++ src/framework/mod.rs
@@ -27,9 +27,9 @@ async fn on_error(error: poise::FrameworkError<'_, Data, BotError>) {
     // They are many errors that can occur, so we only handle the ones we want to customize
     // and forward the rest to the default handler
     match error {
-        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
+        poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {}", error),
         poise::FrameworkError::Command { error, ctx, .. } => {
-            error!("Error in command `{}`: {:?}", ctx.command().name, error,);
+            error!("Error in command `{}`: {}", ctx.command().name, error);
         }
         error => {
             if let Err(e) = poise::builtins::on_error(error).await {
