422c5c41ce76d880848355e6e02f4286f1cbb9ec
Xerxes-2 <dspxue@gmail.com>
2025-07-05 19:25:59 +1000

    feat: Integrate crossbeam for message queue management and batch insertion

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,7 @@
 use std::{path::Path, sync::LazyLock};
 
 use clap::Parser;
+use crossbeam::queue::ArrayQueue;
 use sea_orm::{Database, DatabaseConnection, DbErr};
 
 use crate::Args;
@@ -17,22 +18,35 @@ pub static DB: LazyLock<BotDatabase> = LazyLock::new(|| {
 
 pub struct BotDatabase {
     db: DatabaseConnection,
+    message_insert_queue: ArrayQueue<entity::messages::ActiveModel>,
 }
+
+const DEFAULT_QUEUE_SIZE: usize = 100;
 
 impl BotDatabase {
     pub async fn new(path: impl AsRef<Path>) -> Result<Self, DbErr> {
         let database_url = format!("sqlite://{}", path.as_ref().display());
         let db = Database::connect(&database_url).await?;
 
-        Ok(BotDatabase { db })
+        Ok(BotDatabase {
+            db,
+            message_insert_queue: ArrayQueue::new(DEFAULT_QUEUE_SIZE),
+        })
     }
 
     pub async fn new_memory() -> Result<Self, DbErr> {
         let db = Database::connect("sqlite::memory:").await?;
-        Ok(BotDatabase { db })
+        Ok(BotDatabase {
+            db,
+            message_insert_queue: ArrayQueue::new(DEFAULT_QUEUE_SIZE),
+        })
     }
 
     pub fn inner(&self) -> &DatabaseConnection {
         &self.db
+    }
+
+    pub fn message_queue(&self) -> &ArrayQueue<entity::messages::ActiveModel> {
+        &self.message_insert_queue
     }
 }
