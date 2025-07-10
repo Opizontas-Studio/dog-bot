cd84a6fcbabb3a2c99dd940cb2ff7b1a604a6d0b
Xerxes-2 <dspxue@gmail.com>
2025-07-10 10:05:37 +1000

    feat: implement PingHandler for bot event handling

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,11 +1,11 @@
 mod active;
 mod cookie;
 mod flush;
-mod ping;
+mod boot;
 mod tree_hole;
 
 pub use active::ActiveHandler;
 pub use cookie::CookieHandler;
 pub use flush::FlushHandler;
-pub use ping::PingHandler;
+pub use boot::PingHandler;
 pub use tree_hole::TreeHoleHandler;
