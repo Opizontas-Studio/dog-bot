32eacaf0e4c939ac2b3d99001157e7cc8aa3194b
Xerxes-2 <dspxue@gmail.com>
2025-07-31 09:45:38 +1000

    refactor: streamline Client initialization in main function

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ async fn main() -> Result<(), BotError> {
     let db = BotDatabase::new(&Args::parse().db).await?;
     let cfg = Arc::new(ArcSwap::from_pointee(cfg));
 
-    let mut client = Client::builder(&cfg.load().token, intents)
+    Client::builder(&cfg.load().token, intents)
         .type_map_insert::<BotDatabase>(db.to_owned())
         .type_map_insert::<BotCfg>(cfg.to_owned())
         .event_handler(BootHandler)
@@ -56,11 +56,8 @@ async fn main() -> Result<(), BotError> {
         .event_handler(FlushHandler)
         .event_handler(ActiveHandler)
         .framework(framework(db, cfg))
+        .await?
+        .start()
         .await?;
-
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform exponential backoff until
-    // it reconnects.
-    Ok(client.start().await?)
+    Ok(())
 }
